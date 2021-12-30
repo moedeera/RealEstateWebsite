326e3f8befcaaadfc301f3ba182a3362fb255b89
Moe Deera <deeramoe@gmail.com>
2021-12-30 06:21:57 -0600

    commit

---
+++ routes/api/listings.js
@@ -0,0 +1,26 @@
+const listings =[
+    {
+    address:'50103 Bell Ave',
+    price: 530000,
+    picture1:'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    picture2:'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    picture3:'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    sold:false,
+    owner:'Corey Smith'},
+    {
+    address:'700 North Street',
+    price:890000,
+    picture1:'https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    picture2:'https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    picture3:'https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    sold:false,
+    owner:'John Jones',},
+    
+    {
+    address:'1003 West Blv',
+    price:990000,
+    picture1:'url',
+    picture2:'url',
+    picture3:'url',
+    sold:false,
+    owner:'Gabe Johnson'}]
